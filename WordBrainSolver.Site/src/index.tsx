251140771eed2acb3bb6b2a2170cdeb205d0dd4a
simondevries <devries.simon94@gmail.com>
2017-10-10 22:12:21 +1300

    Modularized react

--- WordBrainSolver.Site/src/index.tsx
+++ WordBrainSolver.Site/src/index.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router';
 
-import {Home} from './app/home';
+import {HomePage} from './app/homePage';
 
 import './index.less';
 import '../images/background.jpg';
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path='/' component={Home}/>
+    <Route path='/' component={HomePage}/>
   </Router>,
   document.getElementById('root')
 );
